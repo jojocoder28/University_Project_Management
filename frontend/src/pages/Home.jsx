bf839cc6775c1e1e95b51172dd223763666ab982
jojocoder28 <dasjojo7@gmail.com>
2024-05-20 16:01:54 +0530

    changes in app.css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Biography from '../components/Biography';
 const Home = () => {
   return ( <>
     <Hero title={"Welcome to UniFolio, the ultimate project management platform for university students"} imageUrl={"/hero1.png"} />
+    <button className="btn glass">Glass button</button>
     <Biography />
     </>
   )
