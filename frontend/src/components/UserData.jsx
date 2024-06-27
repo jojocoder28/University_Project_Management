0664886e7b7a9b71a782ec443b59eeddf254c72f
jojocoder28 <dasjojo7@gmail.com>
2024-06-27 19:58:39 +0530

    Dashboard updated with profile picture edit button

--- frontend/src/components/UserData.jsx
+++ frontend/src/components/UserData.jsx
@@ -16,7 +16,7 @@ function UserData(props) {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [avatar, setAvatar] = useState("");
     const [avatarPreview, setAvatarPreview] = useState("");
-
+    const {user, setUser} = useContext(Context);
     const navigateTo = useNavigate();
     
     const openModal = () => {
@@ -62,8 +62,8 @@ function UserData(props) {
     return (
         <div className="flex flex-col lg:flex-row justify-between mx-5 my-10 p-5 rounded-lg w-full">
             <div className="flex flex-row userData mb-5 lg:mb-0">
-                <div class="relative">
-                    <img class="w-20 h-20 rounded-full" src="blankAvatar.jpg" alt=""/>
+                <div className="relative">
+                    <img className="w-20 h-20 rounded-full" src="blankAvatar.jpg" alt=""/>
                     <div className="flex justify-center items-center">
                     <button className="flex bg-slate-800 w-full lg:mx-4 text-sm py-1 justify-center items-center rounded-lg mt-4 sm:mx-0 md:mx-0" onClick={openModal}> Edit </button>
                     </div>
@@ -84,18 +84,20 @@ function UserData(props) {
                         </div>
                         
                         <div className="flex items-center justify-center w-full px-3 pt-5">
-                            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500">
+                           <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500">
+
                                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                     <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                                     </svg>
+
                                     <p className="text-sm overflow-hidden text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                    <p className="text-xs overflow-hidden text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF</p>
+                                    <p className="text-xs overflow-hidden text-gray-500 dark:text-gray-400">Webp, PNG or JPG</p>
                                 </div>
-                                <input id="dropzone-file" type="file" className="hidden" />
+                                <input id="dropzone-file" type="file" className="hidden" onChange={handleAvatar}/>
                             </label>
                         </div> 
-
+                        <input type="hidden" value={user._id} name="email"></input>
                         <div className="flex justify-center items-center pt-5">
                             <button className="btn btn-primary hover:bg-blue-200 dark:hover:bg-slate-900 shadow-md">Update Image</button>
                         </div>
