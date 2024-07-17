8f26aa2b09f75159004aec5df31efcb9183e0506
jojocoder28 <dasjojo7@gmail.com>
2024-07-17 11:55:21 +0530

    deployment tried vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'esbuild',
+    sourcemap: false,
+  },
   server: {
     port: process.env.PORT || 3000,
     host: '0.0.0.0',
