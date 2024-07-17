6f0afecec8a38139dd349ec48fa34cec507800f3
su1nta <sukanta.developer@gmail.com>
2024-07-18 00:58:37 +0530

    university admin panel added

---
+++ university_admin/src/components/QuickActions.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FaPlusSquare } from "react-icons/fa";
+import { LuPackage } from "react-icons/lu";
+
+
+function QuickActions() {
+    
+    return (
+        <>
+            <div className="flex flex-row items-center gap-4">
+                <div className="text-xl text-slate-200">Quick Actions</div>
+                <button className="btn" onClick={()=>{document.getElementById('StudentModal').showModal() }}>
+                <FaPlusSquare />
+                Add Student
+                </button>
+
+                <dialog id="StudentModal" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Add a Student</h3>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                </div>
+                </dialog>
+
+                <button className="btn" onClick={()=>{document.getElementById('ProjectModal').showModal() }}>
+                <LuPackage />
+                Project Action
+                </button>
+
+                <dialog id="ProjectModal" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Approve/Reject a Project</h3>
+                    <input type="text" placeholder="Project ID" className="input input-bordered w-full max-w-xs mt-4" />
+                    <button className="btn join-item ml-2">Search</button>
+                </div>
+                </dialog>
+
+
+            </div>
+        </>
+    )
+}
+
+export default QuickActions
---
+++ university_admin/src/components/Tabs.jsx
@@ -0,0 +1,38 @@
+// src/components/Tabs.js
+import React, { useState } from 'react';
+import Table from './Table';
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="container mt-10 p-4">
+      <div role="tablist" className="tabs tabs-boxed">
+        <a
+          role="tab"
+          className={`tab tab-bordered ${activeTab === 0 ? 'tab-active' : ''}`}
+          onClick={() => handleTabClick(0)}
+        >
+          New Projects
+        </a>
+        <a
+          role="tab"
+          className={`tab tab-bordered ${activeTab === 1 ? 'tab-active' : ''}`}
+          onClick={() => handleTabClick(1)}
+        >
+          Students Enrolled
+        </a>
+      </div>
+      <div className="mt-4">
+        {activeTab === 0 && <div><Table title="Project" th={["Project Name", "Tags", "Contributors"]} /></div>}
+        {activeTab === 1 && <div><Table title="Students" th={["Name", "Department", "Niche"]} /></div>}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
