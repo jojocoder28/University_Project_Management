f7bc66d0cc95fcc8e747dd42e82248ec8433c4cb
jojocoder28 <dasjojo7@gmail.com>
2024-07-19 17:54:55 +0530

    student update by admin modified

--- university_admin/src/components/StudentCard.jsx
+++ university_admin/src/components/StudentCard.jsx
@@ -1,19 +1,16 @@
-import React , {useState, useEffect, useContext} from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
 import UpdateStudentDetails from './UpdateStudentDetails';
 import { toast } from "react-toastify";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import backend_api from '../config';
 
-
-
 const StudentCard = ({ student }) => {
-
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isModalOpen2, setIsModalOpen2] = useState(false);
     const [isDataAvailable, setIsDataAvailable] = useState(false);
-    const [email,setEmail] = useState(student.email);
+    const [email, setEmail] = useState(student.email);
     const navigateTo = useNavigate();
 
     const openModal = () => {
@@ -31,96 +28,96 @@ const StudentCard = ({ student }) => {
     const closeModal2 = () => {
         setIsModalOpen2(false);
     };
-    
-    useEffect(() => {
-        if (student.rollnumber && student.admissiondate) {
-          setIsDataAvailable(true);
-        } else {
-          setIsDataAvailable(false);
-        }
-      }, [isModalOpen]);
 
     const updateOption = () => {
         setIsDataAvailable(false);
-    }
+    };
 
     const handleDelete = async (e) => {
         e.preventDefault();
         try {
-          await axios
-            .post(
-              backend_api+"api/v1/university/user/delete",
-              { email },
-              {
-                withCredentials: true,
-                headers: { "Content-Type": "application/json" },
-              }
-            )
-            .then((res) => {
-              toast.success(res.data.message);
-              navigateTo("/students")
-              setIsModalOpen2(false)
-              setEmail("");    
-            });
+            await axios.post(
+                backend_api + "api/v1/university/user/delete",
+                { email },
+                {
+                    withCredentials: true,
+                    headers: { "Content-Type": "application/json" },
+                }
+            );
+            toast.success('Student deleted successfully');
+            navigateTo("/students");
+            setIsModalOpen2(false);
+            setEmail("");
         } catch (error) {
-          toast.error(error.response.data.message);
+            toast.error(error.response.data.message);
         }
-      };
-    
-  return (
-    <>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-slate-800 transform hover:scale-105 transition-transform duration-300">
-      <img className="w-full h-48 object-cover" src={student.avatar && student.avatar.url || 'blankAvatar.jpg'} alt={`${student.username}'s profile`} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{student.firstName+" "+student.lastName}</div>
-        <p className="text-gray-300 text-base">Email: {student.email}</p>
-        <p className="text-gray-300 text-base">Username: {student.username}</p>
-      </div>
-      <div className="px-6 pt-4 pb-2 flex justify-between items-center">
-        <button className="bg-slate-500 hover:bg-green-700 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full" onClick={openModal}>
-          Edit
-        </button>
-        <div className='px-1'>&nbsp;</div>
-        <button className="bg-red-500 hover:bg-red-800 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full" onClick={openModal2}>
-          Delete
-        </button>
-      </div>
-    </div>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-            {
-                isDataAvailable ? (
-                    <div className='flex flex-col justify-center items-center align-middle'>
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">{student.firstName+" "+student.lastName}</div>
-                            <p className="text-gray-300 text-base">Email: {student.email}</p>
-                            <p className="text-gray-300 text-base">Username: {student.username}</p>
-                            <p className="text-gray-300 text-base">Roll Number: {student.rollnumber}</p>
-                            <p className="text-gray-300 text-base">Admission Date: {student.admissiondate}</p>
-                        </div>
-                        <button className='btn btn-primary' onClick={updateOption}>Edit</button>
-                    </div>
-                ) : (
-                    <UpdateStudentDetails email={student.email} name={student.firstName+" "+student.lastName} />
-                )
-            }
-        </Modal>
-        <Modal isOpen={isModalOpen2} onClose={closeModal2}>
-            <div className="flex flex-col justify-center items-center">
-                <p className='text-xl'>Are you sure you want to delete this student?</p>
-                <div className="flex flex-row">
-                    <div className="px-4 pt-5">
-                <button className='bg-red-500 hover:bg-red-800 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full' onClick={handleDelete}>Yes</button>
+    };
+
+    // Callback function to handle successful update
+    const handleUpdateSuccess = () => {
+        closeModal();
+        setIsDataAvailable(true);
+    };
+
+    return (
+        <>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-slate-800 transform hover:scale-105 transition-transform duration-300">
+                <img className="w-full h-48 object-cover" src={student.avatar && student.avatar.url || 'blankAvatar.jpg'} alt={`${student.username}'s profile`} />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{student.firstName + " " + student.lastName}</div>
+                    <p className="text-gray-300 text-base">Email: {student.email}</p>
+                    <p className="text-gray-300 text-base">Username: {student.username}</p>
                 </div>
-                <div className="px-4 pt-5">
-                <button className="bg-slate-500 hover:bg-green-700 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full" onClick={closeModal2}>
-                    No
-                </button>
-                </div>
+                <div className="px-6 pt-4 pb-2 flex justify-between items-center">
+                    <button className="bg-slate-500 hover:bg-green-700 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full" onClick={openModal}>
+                        Edit
+                    </button>
+                    <button className="bg-red-500 hover:bg-red-800 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full" onClick={openModal2}>
+                        Delete
+                    </button>
                 </div>
             </div>
-        </Modal>
+
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                {
+                    isDataAvailable ? (
+                        <div className='flex flex-col justify-center items-center align-middle'>
+                            <div className="px-6 py-4">
+                                <div className="font-bold text-xl mb-2">{student.firstName + " " + student.lastName}</div>
+                                <p className="text-gray-300 text-base">Email: {student.email}</p>
+                                <p className="text-gray-300 text-base">Username: {student.username}</p>
+                                <p className="text-gray-300 text-base">Roll Number: {student.rollnumber}</p>
+                                <p className="text-gray-300 text-base">Admission Date: {student.admissiondate}</p>
+                            </div>
+                            <button className='btn btn-primary' onClick={updateOption}>Edit</button>
+                        </div>
+                    ) : (
+                        <UpdateStudentDetails
+                            email={student.email}
+                            name={student.firstName + " " + student.lastName}
+                            onUpdateSuccess={handleUpdateSuccess}
+                        />
+                    )
+                }
+            </Modal>
+
+            <Modal isOpen={isModalOpen2} onClose={closeModal2}>
+                <div className="flex flex-col justify-center items-center">
+                    <p className='text-xl'>Are you sure you want to delete this student?</p>
+                    <div className="flex flex-row">
+                        <div className="px-4 pt-5">
+                            <button className='bg-red-500 hover:bg-red-800 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full' onClick={handleDelete}>Yes</button>
+                        </div>
+                        <div className="px-4 pt-5">
+                            <button className="bg-slate-500 hover:bg-green-700 border-none text-white font-bold py-2 px-4 rounded-md mb-4 w-full" onClick={closeModal2}>
+                                No
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </>
-  );
+    );
 };
 
 export default StudentCard;
--- university_admin/src/components/UpdateStudentDetails.jsx
+++ university_admin/src/components/UpdateStudentDetails.jsx
@@ -1,81 +1,69 @@
 import axios from "axios";
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import backend_api from "../config";
 
-
-const UpdateStudentDetails = (props) => {
+const UpdateStudentDetails = ({ email, name, onUpdateSuccess }) => {
     const [rollnumber, setRollnumber] = useState("");
     const [admissiondate, setadmissiondate] = useState("");
-    const [email, setemail] = useState(props.email);
-  
-    const navigateTo = useNavigate()
-    
-  
+    const [emailState, setEmailState] = useState(email);
+    const navigateTo = useNavigate();
+
     const handleUpdate = async (e) => {
-      e.preventDefault();
-      try {
-        await axios
-          .post(
-            backend_api+"api/v1/university/user/update",
-            { email, admissiondate, rollnumber },
-            {
-              withCredentials: true,
-              headers: { "Content-Type": "application/json" },
-            }
-          )
-          .then((res) => {
-            toast.success(res.data.message);
-            navigateTo("/students");
+        e.preventDefault();
+        try {
+            const response = await axios.post(
+                backend_api + "api/v1/university/user/update",
+                { email: emailState, admissiondate, rollnumber },
+                {
+                    withCredentials: true,
+                    headers: { "Content-Type": "application/json" },
+                }
+            );
+            toast.success(response.data.message);
+            onUpdateSuccess(); // Notify the parent component of success
             setRollnumber("");
             setadmissiondate("");
-            setemail("");
-  
-          });
-      } catch (error) {
-        toast.error(error.response.data.message);
-      }
+            setEmailState("");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
     };
-  return (
-    <>
-        
-            <div className="flex flex-col justify-center items-center pb-5">
-            <p className="font-mono text-4xl text-yellow-50">{props.name}</p>
-              <div>
-                <h2 className="text-center text-3xl font-extrabold py-5">Update Student Details</h2>
-              </div>
-              <form className="w-full max-w-lg" onSubmit={handleUpdate}>
-                
-                <div className="flex flex-wrap -mx-3 mb-6">
-                  <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide dark:text-white text-gray-700 text-xs font-bold mb-2" htmlFor="grid-rollnumber">
-                      Roll Number
-                    </label>
-                    <input value={rollnumber} onChange={(e) => setRollnumber(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-rollnumber" type="test" placeholder="university roll number"/>
-                    
-                  </div>
-                </div>
-                
-                <div className="flex flex-wrap -mx-3 mb-6">
-                  <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide dark:text-white text-gray-700 text-xs font-bold mb-2" htmlFor="grid-admissiondate">
-                      admissiondate
-                    </label>
-                    <input value={admissiondate} onChange={(e) => setadmissiondate(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-admissiondate" type="date"/>
-                  </div>
-                </div>
-                 
-                
-                <div className="flex justify-center pt-2 items-center">
-                  <button className="w-full flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded items-center justify-center" type="submit">
-                    Update
-                  </button>
-                </div>
-              </form>
-            </div>
-    </>
-  )
-}
 
-export default UpdateStudentDetails
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center pb-5">
+                <p className="font-mono text-4xl text-yellow-50">{name}</p>
+                <div>
+                    <h2 className="text-center text-3xl font-extrabold py-5">Update Student Details</h2>
+                </div>
+                <form className="w-full max-w-lg" onSubmit={handleUpdate}>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide dark:text-white text-gray-700 text-xs font-bold mb-2" htmlFor="grid-rollnumber">
+                                Roll Number
+                            </label>
+                            <input value={rollnumber} onChange={(e) => setRollnumber(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-rollnumber" type="text" placeholder="university roll number" />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide dark:text-white text-gray-700 text-xs font-bold mb-2" htmlFor="grid-admissiondate">
+                                Admission Date
+                            </label>
+                            <input value={admissiondate} onChange={(e) => setadmissiondate(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-admissiondate" type="date" />
+                        </div>
+                    </div>
+                    <div className="flex justify-center pt-2 items-center">
+                        <button className="w-full flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded items-center justify-center" type="submit">
+                            Update
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default UpdateStudentDetails;
