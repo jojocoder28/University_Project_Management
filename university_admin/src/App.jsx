760662c1fc9ff06ec815d09858bef959cca93737
jojocoder28 <dasjojo7@gmail.com>
2024-07-19 15:40:07 +0530

    process env introduced

--- university_admin/src/App.jsx
+++ university_admin/src/App.jsx
@@ -15,6 +15,7 @@ import Loading from './components/Loading';
 import { Context } from "./main";
 import Login from './pages/Login';
 import Students from './pages/Students';
+import backend_api from './config';
 
 const App = () => {
   const { isAuthenticated, setIsAuthenticated, admin, setAdmin } =
@@ -27,7 +28,7 @@ const App = () => {
       setIsLoading(true);
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/university/admin/universityadmin/getall",
+          backend_api+"api/v1/university/admin/universityadmin/getall",
           {
             withCredentials: true,
           }
