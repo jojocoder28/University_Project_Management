69dede245ec93e7e9af4c8ad51af357acb1d8337
jojocoder28 <dasjojo7@gmail.com>
2024-07-20 12:39:54 +0530

    about us page modified

---
+++ frontend/src/components/Avatar.jsx
@@ -0,0 +1,90 @@
+import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+const Avatar = (props) => {
+    const { nodes, materials } = useGLTF("models/swarnadeep.glb");
+    const group = useRef();
+    const lightRef = useRef();
+  
+    useFrame((state) => {
+      const target = new THREE.Vector3(state.mouse.x * 10, state.mouse.y * 10, 5);
+      lightRef.current.position.lerp(target, 0.1); // Smoothly move the light to the target position
+      group.current.getObjectByName("Spine2").lookAt(target);
+      
+    });
+  
+    return (
+      <group {...props} ref={group} dispose={null}>
+        {/* Light that follows the mouse */}
+        <pointLight ref={lightRef} intensity={150} distance={100} />
+        
+        {/* Avatar elements */}
+        <group>
+          <primitive object={nodes.Hips} />
+          <skinnedMesh
+            geometry={nodes.Wolf3D_Body.geometry}
+            material={materials.Wolf3D_Body}
+            skeleton={nodes.Wolf3D_Body.skeleton}
+          />
+          <skinnedMesh
+            geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+            material={materials.Wolf3D_Outfit_Bottom}
+            skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+          />
+          <skinnedMesh
+            geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+            material={materials.Wolf3D_Outfit_Footwear}
+            skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+          />
+          <skinnedMesh
+            geometry={nodes.Wolf3D_Outfit_Top.geometry}
+            material={materials.Wolf3D_Outfit_Top}
+            skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+          />
+          <skinnedMesh
+            geometry={nodes.Wolf3D_Hair.geometry}
+            material={materials.Wolf3D_Hair}
+            skeleton={nodes.Wolf3D_Hair.skeleton}
+          />
+          <skinnedMesh
+            name="EyeLeft"
+            geometry={nodes.EyeLeft.geometry}
+            material={materials.Wolf3D_Eye}
+            skeleton={nodes.EyeLeft.skeleton}
+            morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+            morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+          />
+          <skinnedMesh
+            name="EyeRight"
+            geometry={nodes.EyeRight.geometry}
+            material={materials.Wolf3D_Eye}
+            skeleton={nodes.EyeRight.skeleton}
+            morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+            morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+          />
+          <skinnedMesh
+            name="Wolf3D_Head"
+            geometry={nodes.Wolf3D_Head.geometry}
+            material={materials.Wolf3D_Skin}
+            skeleton={nodes.Wolf3D_Head.skeleton}
+            morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+            morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+          />
+          <skinnedMesh
+            name="Wolf3D_Teeth"
+            geometry={nodes.Wolf3D_Teeth.geometry}
+            material={materials.Wolf3D_Teeth}
+            skeleton={nodes.Wolf3D_Teeth.skeleton}
+            morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+            morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+          />
+        </group>
+      </group>
+    );
+  };
+  
+  export default Avatar;
+  
+  useGLTF.preload("models/swarnadeep.glb");
--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  Sky,
+} from "@react-three/drei";
+import Avatar from '../components/Avatar';
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className="container mx-auto flex w-screen h-screen justify-center lg:justify-start mt-5">
+    <Canvas shadows camera={{ position: [0, 1, 5], fov: 30 }}>
+      {/* <color attach="background" args={["#ececec"]} /> */}
+      <OrbitControls />
+      {/* <Sky /> */}
+      {/* <Environment preset="sunset" /> */}
+      <group position-y={-1}>
+        <ContactShadows
+          opacity={0.42}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
+        <Avatar />
+      </group>
+    </Canvas>
+    <div className='flex flex-col overflow-hidden absolute mt-2 mx-auto lg:mx-12 uppercase'>
+      <h1 className="overflow-hidden font-extrabold text-4xl">Swarnadeep Das</h1>
+      <h1 className="overflow-hidden font-extrabold text-xl">MCA 2023-25</h1>
+      <h1 className="overflow-hidden font-extrabold text-2xl">University of Calcutta</h1>
+      <div className='static lg:fixed bottom-10 z-10'>
+        <h1 className="overflow-hidden font-extrabold text-4xl uppercase">Team <span className='text-teal-500 dark:text-teal-400'>Lead</span></h1>
+      </div>
+    </div>
+    </div>
   )
 }
 
